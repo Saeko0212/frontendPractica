fe546410d01d49f0a907e3b99585f75cdf09146b
Saeko0212 <derlingcruz2@gmail.com>
2025-05-26 16:26:18 -0600

    listo

--- src/views/productos.jsx
+++ src/views/productos.jsx
@@ -5,6 +5,8 @@ import { Container, Button, Row, Col } from "react-bootstrap";
 import ModalEliminacionProducto from '../components/producto/ModalEliminacionProducto';
 import ModalEdicionProducto from '../components/producto/ModalEdicionProductos';
 import CuadroBusquedas from '../components/busquedas/busquedas';
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const productos = () => {
   const [listaProductos, setListaProductos] = useState([]);
@@ -15,6 +17,7 @@ const productos = () => {
   const [mostrarModal, setMostrarModal] = useState(false);
   const [mostrarModalEliminacion, setMostrarModalEliminacion] = useState(false);
 const [productoAEliminar, setProductoAEliminar] = useState(null);
+
 
   const [nuevoProducto, setNuevoProducto] = useState({
     nombre_producto: '',
@@ -189,6 +192,63 @@ const abrirModalEdicion = (producto) => {
     setProductosFiltrados(filtrados);
   };
   
+  const generarPDFProductos = () => {
+    const doc = new jsPDF();
+
+    doc.setFillColor(48, 41, 51);
+    doc.rect(0,0,220,30,'F');
+    doc.setTextColor(255, 255, 255);
+    doc.setFontSize(28);
+    doc.text("Lista de productos", doc.internal.pageSize.getWidth()/ 2, 18,{align: "center"});const columnas = ["ID","Nombre", "Descripcion", "Categoria", "Precio", "Stock"];
+const filas = productosFiltrados.map((producto)=>[
+  producto.id_producto,
+  producto.nombre_producto,
+  producto.descripcion_producto,
+  producto.id_categoria,
+  `C$ ${producto.precio_unitario}`,
+  producto.stock,
+]);
+const totalPaginas = "{total_pages_count_string}";
+
+autoTable(doc,{
+  head: [columnas],
+  body: filas,
+  startY: 40,
+  theme: "grid",
+  styles: { fontSize: 10, cellPadding: 2},
+  margin:{top: 20, left: 14, right: 14},
+  tableLineWidth: "auto",
+  columnStyles: {
+    0: {cellWidth: 'auto'},
+    1: {cellWidth: 'auto'},
+    2: {cellWidth: 'auto'},
+  },
+  pageBreak: "auto",
+  rowPageBreak: "auto",
+
+  didDrawPage: function(data){
+    const alturaPagina = doc.internal.pageSize.getHeight();
+    const anchoPagina = doc.internal.pageSize.getWidth();
+    const numeroPagina = doc.internal.getNumberOfPages();
+
+    doc.setFontSize(10);
+    doc.setTextColor(0, 0, 0);
+    const piePagina = `Pagina ${numeroPagina} de ${totalPaginas}`;
+    doc.text(piePagina, anchoPagina / 2 + 15, alturaPagina - 10, {align: "center"});
+  },
+});
+if (typeof doc.putTotalPages === 'function'){
+  doc.putTotalPages(totalPaginas);
+}
+
+const fecha = new Date();
+const dia = String(fecha.getDate()).padStart(2, '0');
+const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+const anio = fecha.getFullYear();
+const nombreArchivo = `productos_${dia}${mes}${anio}.pdf`
+doc.save(nombreArchivo);
+
+  }
 
   return (
     <Container className="mt-5">
@@ -206,6 +266,16 @@ const abrirModalEdicion = (producto) => {
         manejarCambioBusqueda={manejarCambioBusqueda}
       />
     </Col>
+    <col lg={3} md={4} sm={4} xs={5}>
+    <Button 
+     className="mb-3"
+     onClick={generarPDFProductos}
+     variant="secondary"
+     style={{width: "100%" }}
+     >
+      Generar reporte PDF
+    </Button>
+    </col>
   </Row>
       <br/><br/>
 
